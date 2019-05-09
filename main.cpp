f2c4d4b5d4a71dfdd02257f922918957c8b9bd7a
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-05-08 19:34:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,25 +26,10 @@ int main(){
 	num2 = 555;
 	cout << "value of num2 is " << num2 << endl;
 	cout <<"Value of ptr is " << *ptr << endl;
-	cou t<<"Value of ptr is " << ptr << endl;
-	//add_numbers(num1,1);
-	//cout << "Adding 5 to 300: " << num1 << endl;
-	//add_numbers(num1,num2)
-	//cout << "Adding 5 to 300: " << num1 << endl;
-	//add_numbers(num1,num2)
-	//cout << "Adding 5 to 300: " << num1 << endl;
+	cout<<"Value of ptr is " << ptr << endl;
+	
 	
 	return 0;
 }
 
 
-//bool TRUE = true;
-	//if(DEBUG){ cout << "Value of TRUE: " << TRUE << endl; }
-	
-	
-	//cout << "this is something to the screen." << endl;
-	//if(TRUE){
-	//	cout << "....and more to the screen." << endl;
-	//} else {
-	//	cout << "...else less to screen." << endl;
-	//}
